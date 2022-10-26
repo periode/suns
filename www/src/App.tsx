52b667c7a37edd3a46fe366b572b346c1c389cfd
période <pierre.depaz@gmail.com>
2022-10-26 17:50:15 +0200

    added basic map

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -1,18 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
+import { ImageOverlay, MapContainer, Marker, Popup, useMap } from 'react-leaflet'
+import { CRS } from 'leaflet';
+import L from "leaflet";
 
 function App() {
-
+  const b = new L.LatLngBounds(new L.LatLng(0, 0), new L.LatLng(500, 500))
   useEffect(() => {
     console.log(`do API call here`);
-    
   })
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Yes papi</p>
-      </header>
+      <div className="map-container" id="map">
+        <MapContainer center={[250, 250]} minZoom={2} maxZoom={8} zoom={2} scrollWheelZoom={true} crs={CRS.Simple} maxBounds={b} inertia={false}>
+          <ImageOverlay url="https://i.imgur.com/Ion6X7C.jpg" bounds={b}/>
+          <Marker position={[250, 250]}>
+            <Popup>
+              Hello.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
     </div>
   );
 }
