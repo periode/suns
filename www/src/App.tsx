d4f2f7d8b3ae6b887eaa73fe89cf89ade31456d7
période <pierre.depaz@gmail.com>
2022-10-26 19:00:11 +0200

    refactored bounds var

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -12,7 +12,7 @@ interface ClusterInterface {
 
 const App = () => {
   const [clusters, setClusters] = useState(Array<ClusterInterface>)
-  const b = new L.LatLngBounds(new L.LatLng(0, 0), new L.LatLng(500, 500))
+  const bounds = new L.LatLngBounds(new L.LatLng(0, 0), new L.LatLng(500, 500))
 
   useEffect(() => {
     async function fetchClusters() {
@@ -27,16 +27,11 @@ const App = () => {
     fetchClusters()
   }, [])
 
-  useEffect(() => {
-    console.log(`clusters: ${clusters.length}`);
-
-  }, [clusters])
-
   return (
     <div className="App">
       <div className="map-container" id="map">
-        <MapContainer center={[250, 250]} minZoom={2} maxZoom={8} zoom={2} scrollWheelZoom={true} crs={CRS.Simple} maxBounds={b} inertia={false}>
-          <ImageOverlay url="https://i.imgur.com/Ion6X7C.jpg" bounds={b} />
+        <MapContainer center={[250, 250]} minZoom={2} maxZoom={8} zoom={2} scrollWheelZoom={true} crs={CRS.Simple} maxBounds={bounds} inertia={false}>
+          <ImageOverlay url="https://i.imgur.com/Ion6X7C.jpg" bounds={bounds} />
           <>
             {clusters.map(c => {
               return (
