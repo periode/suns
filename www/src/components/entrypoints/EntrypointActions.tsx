a282302d3bcb897e6c29e64864bb620a11db9cce
période <pierre.depaz@gmail.com>
2022-11-11 14:18:00 +0100

    fixed users length bug and flex positioning on entrypoint actions

--- www/src/components/entrypoints/EntrypointActions.tsx
+++ www/src/components/entrypoints/EntrypointActions.tsx
@@ -3,10 +3,10 @@ import { FiShare2, FiArrowRight } from "react-icons/fi"
 
 interface EntrypointActionsProps {
 	entryPointData: IEntrypoint,
-	isOwner : boolean,
-	session : Object,
-	claimEntryPointFunction : () => {},
-	completeModuleFunction : (data : any, session : Object) => Promise<void>,
+	isOwner: boolean,
+	session: Object,
+	claimEntryPointFunction: () => {},
+	completeModuleFunction: (data: any, session: Object) => Promise<void>,
 }
 
 function EntrypointActions({
@@ -15,78 +15,74 @@ function EntrypointActions({
 	completeModuleFunction,
 	isOwner,
 	session
-} : EntrypointActionsProps) {
+}: EntrypointActionsProps) {
 
-	const copyToClipboard = (text : string) => {
+	const copyToClipboard = (text: string) => {
 		window.prompt("You can share this link: ", text);
 	}
 
-	console.log(entryPointData.status, isOwner);
-	
-
 	const handleNext = () => {
-		completeModuleFunction(entryPointData, session)
+		console.log('clicking next')
+		// completeModuleFunction(entryPointData, session)
 	}
-	
-	const ShareButton = 
-		<div    className=" font-mono
+
+	const ShareButton =
+		<div className=" font-mono
 							cursor-pointer
 							flex items-center
 							gap-1"
-                onClick={ () => { copyToClipboard( window.location.href ) } }>
-            <FiShare2 className="text-xs"/>
+			onClick={() => { copyToClipboard(window.location.href) }}>
+			<FiShare2 className="text-xs" />
 			<p>Share</p>
-        </div>
-
-	const StartButton =
-		<div    className=" font-mono
-							cursor-pointer
-							flex items-center
-							gap-1"
-                onClick={ claimEntryPointFunction }>
-			<p>Start</p>
-            <FiArrowRight className="text-xs"/>
-        </div>
-
-	const NextButton =
-		<div    className=" font-mono
-							cursor-pointer
-							flex items-center
-							gap-1"
-                onClick={ handleNext }>
-			<p>Next</p>
-            <FiArrowRight className="text-xs"/>
-        </div>
-
-	const FinishButton =
-		<div	className=" font-mono
-							cursor-pointer
-							flex items-center
-							gap-1"
-				onClick={ handleNext }>
-			<p>Finish</p>
-			<FiArrowRight className="text-xs"/>
 		</div>
 
-	const Step = 
-	<p className="absolute 
+	const StartButton =
+		<div className=" font-mono
+							cursor-pointer
+							flex items-center
+							gap-1"
+			onClick={claimEntryPointFunction}>
+			<p>Start</p>
+			<FiArrowRight className="text-xs" />
+		</div>
+
+	const NextButton =
+		<button className=" font-mono
+							cursor-pointer
+							flex items-center
+							gap-1"
+			onClick={handleNext}>
+			<p>Next</p>
+			<FiArrowRight className="text-xs" />
+		</button>
+
+	const FinishButton =
+		<div className=" font-mono
+							cursor-pointer
+							flex items-center
+							gap-1"
+			onClick={handleNext}>
+			<p>Finish</p>
+			<FiArrowRight className="text-xs" />
+		</div>
+
+	const Step =
+		<p className=" 
 					w-full h-full
 					flex items-center justify-center
 					text-center  font-mono
 					opacity-50">
-			{ entryPointData.current_module } / { entryPointData.modules.length }
-	</p>
+			{entryPointData.current_module} / {entryPointData.modules.length}
+		</p>
 
 	const rightButtonDisplay = () => {
-		if (entryPointData.status === ENTRYPOINT_STATUS.EntrypointPending && isOwner)
-		{
+		if (entryPointData.status === ENTRYPOINT_STATUS.EntrypointPending && isOwner) {
 			if (entryPointData.current_module === entryPointData.modules.length)
 				return FinishButton
 			else
 				return NextButton
-		}	
-		else
-		{
+		}
+		else {
 			if (entryPointData.status === ENTRYPOINT_STATUS.EntrypointOpen)
 				return StartButton
 			else
@@ -95,15 +91,17 @@ function EntrypointActions({
 	}
 
 
-	return ( 
-	<>
-		{	
-			entryPointData.status === ENTRYPOINT_STATUS.EntrypointPending && isOwner &&
-				Step 
-		}
-		{ ShareButton } 
-		{ rightButtonDisplay() }
-	</> 
+	return (
+		<>
+			<div className="flex flex-row justify-between flex-1">
+				{ShareButton}
+				{
+					entryPointData.status === ENTRYPOINT_STATUS.EntrypointPending && isOwner &&
+					Step
+				}
+				{rightButtonDisplay()}
+			</div>
+		</>
 	);
 }
 
