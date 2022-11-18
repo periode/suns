c11559048d3a710667f05cdf3d9f8af6359b09bd
période <pierre.depaz@gmail.com>
2022-11-18 10:20:54 +0100

    added Value field on task and content for backup data

--- api/models/content.go
+++ api/models/content.go
@@ -14,8 +14,9 @@ type Content struct {
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
 	UUID      uuid.UUID      `gorm:"uniqueIndex;type:uuid;primaryKey;default:uuid_generate_v4()" json:"uuid" yaml:"uuid"`
 
-	Type string `json:"type" yaml:"type"`
-	Key  string `json:"key" yaml:"key"`
+	Type  string `json:"type" yaml:"type"`
+	Key   string `json:"key" yaml:"key"`
+	Value string `json:"value" yaml:"value"`
 
 	//-- belongs to a module
 	ModuleUUID uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4()" json:"module_uuid" yaml:"module_uuid"`
