0aab06777e8a470c1d0ee8a4cda874f6a5659f21
période <pierre.depaz@gmail.com>
2022-11-27 01:44:01 +0100

    getting the airtable media content and treating it as an absolute path

--- www/src/components/modules/content/ContentPhoto.tsx
+++ www/src/components/modules/content/ContentPhoto.tsx
@@ -7,7 +7,7 @@ function ContentPhoto({src} : ContentPhotoProps) {
 	return ( 
 		<div className="flex items-center justify-start">
 			<img className="w-auto max-h-80"
-				src={`${process.env.REACT_APP_API_URL}/static/${src}`}
+				src={`${src}`}
 				alt={src}/>
 		</div>
 	 );
