7ae9e91a956c750659c9bb07ad14715e1096f483
Celestin <hello@celest.in>
2022-12-08 04:04:28 +0100

    styling entrypoint

---
+++ www/src/assets/svgs/Octagon.tsx
@@ -0,0 +1,10 @@
+function Octagon() {
+	return ( 
+		<svg className="w-full h-full" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+		<path d="M11.7157 0L28.2843 9.52059e-07L40 11.7157L40 28.2843L28.2843 40L11.7157 40L0 28.2843L5.34181e-07 11.7157L11.7157 0Z"/>
+		</svg>
+
+	 );
+}
+
+export default Octagon;
