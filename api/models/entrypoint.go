6cf58d6a8a979e81786a25ffada66876026c9cc0
période <pierre.depaz@gmail.com>
2022-12-19 16:05:42 +0100

    changed GetMapEntrypoints to not return the sacrificed entrypoints

--- api/models/entrypoint.go
+++ api/models/entrypoint.go
@@ -179,7 +179,7 @@ func GetAllEntrypoints() ([]Entrypoint, error) {
 // -- GetMapEntrypoints returns all entrypoints that are visible on the map and that have modules. It is used to generate the map, to find which entrypoints should be sacrificed, and to determine the creation of new entrypoints.
 func GetMapEntrypoints() ([]Entrypoint, error) {
 	eps := make([]Entrypoint, 0)
-	result := db.Preload("Modules").Preload("Cluster").Preload("Users").Where("visibility = ?", EntrypointVisible).Find(&eps)
+	result := db.Preload("Modules").Preload("Cluster").Preload("Users").Where("visibility = ?", EntrypointVisible).Not("status = ?", EntrypointSacrificed).Find(&eps)
 
 	final := make([]Entrypoint, 0)
 	for _, ep := range eps {
