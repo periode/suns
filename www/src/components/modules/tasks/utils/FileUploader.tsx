4503012f090bad358f500272f9f210724175a5af
Celestin <hello@celest.in>
2022-12-21 17:09:51 +0100

    styled img preview

--- www/src/components/modules/tasks/utils/FileUploader.tsx
+++ www/src/components/modules/tasks/utils/FileUploader.tsx
@@ -55,8 +55,8 @@ const FileUploader = ({ type, uuid, maxUploads = 1, handleNewUploads }: IFileUpl
                     capture="environment"
                     accept={type === "img" ? "image/*" : "video/*"}
                     onChange={handleFileChange}
-                    className="font-mono text-sm w-full bg-amber-100 p-4 flex flex-col gap-4 text-amber-900/50 
-                                file:p-2 file:pr-4 file:pl-4 file:bg-transparent file:border-amber-500 file:text-amber-500 file:font-semibold file:border-[1px] file:shadow-none file:border-solid file:mr-4
+                    className="font-mono text-sm w-full p-4 flex flex-col gap-4 text-amber-900 
+                                file:p-2 file:pr-4 file:pl-4 file:bg-amber-100 file:border-amber-500 file:text-amber-500 file:font-semibold file:border-[1px] file:shadow-none file:border-solid file:mr-4
                                 "
                 />
             )
@@ -65,7 +65,7 @@ const FileUploader = ({ type, uuid, maxUploads = 1, handleNewUploads }: IFileUpl
         return fields
     }
 
-    return (<>
+    return (<div>
         <Toaster
             type={ToasterType.error}
             message={"File is too big, max size: " + MAX_FILE_SIZE[type] + "mb."}
@@ -73,15 +73,27 @@ const FileUploader = ({ type, uuid, maxUploads = 1, handleNewUploads }: IFileUpl
             setDisplay={setIsToasterDisplayed}
             timeoutms={3000}
         />
-        <div className="flex">
-            <div className="flex-1">
-                {getInputFields()}
-            </div>
-            <div className="flex-1 pl-3">
-                {previewSrc !== "" ? <img src={previewSrc} /> : <></>}
-            </div>
+        <div className="relative w-full h-16 bg-amber-100">
+            {
+                    previewSrc !== "" ?
+                    <div className="absolute w-full h-full overflow-hidden">
+                        <img className="absolute w-full h-full 
+                                        object-center object-cover overflow-hidden"
+                            src={previewSrc}
+                            alt="preview">
+                        </img>
+                        <div className="absolute w-full h-full bg-amber-100/80"></div>
+                    </div>
+                    :
+                    <></>
+            }
+            {
+                <div className="absolute w-full h-full">
+                    { getInputFields() }
+                </div>
+            }
         </div>
-    </>)
+    </div>)
 }
 
 export default FileUploader
