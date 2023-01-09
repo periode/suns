1fefb48b33b7d35eb5b624bdac717d1f623560ea
Celestin <hello@celest.in>
2023-01-09 14:31:23 +0100

    changes on public pages

--- www/src/About.tsx
+++ www/src/About.tsx
@@ -1,5 +1,5 @@
 import { useContext } from "react"
-import AirContext, { AirTableContext } from "./contexts/AirContext"
+import { AirTableContext } from "./contexts/AirContext"
 import PulicPageLayout from "./components/entrypoints/Layouts/PublicPageLayout"
 
 
@@ -11,13 +11,27 @@ const About = () => {
     return (
             <PulicPageLayout>
                 <h1>About</h1>
-                <h2>{ contents?.get("about_headline_1")}</h2>
-                <p>{ contents?.get("about_content_1")}</p>
-                <h2>{ contents?.get("about_headline_2")}</h2>
-                <p>{ contents?.get("about_content_2")}</p>
-                <h2>{ contents?.get("about_headline_3")}</h2>
-                <p>{ contents?.get("about_content_3")}</p>
-                <p>okay okay</p>
+                <h2>
+                    { contents?.get("about_headline_1")}
+                </h2>
+                <p>
+                    { contents?.get("about_content_1")}
+                </p>
+                <h2>
+                    { contents?.get("about_headline_2")}
+                </h2>
+                <p>
+                    { contents?.get("about_content_2")}
+                </p>
+                <h2>
+                    { contents?.get("about_headline_3")}
+                </h2>
+                <p>
+                    { contents?.get("about_content_3")}
+                </p>
+                <p>
+                    okay okay
+                </p>
             </PulicPageLayout>
     )
 }
