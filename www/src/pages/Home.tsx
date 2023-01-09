8d3a4df2c359d73390008f51405671d04efa785d
Celestin <hello@celest.in>
2023-01-09 22:44:00 +0100

    added homepage

--- www/src/pages/Home.tsx
+++ www/src/pages/Home.tsx
@@ -11,18 +11,21 @@ function Home() {
 							flex items-center justify-center">
 				<div className="flex flex-col items-center gap-2">
 					<h1 className="text-8xl text-center">Joining Suns</h1>
-					<div className="flex items-center p-4 font-mono gap-4 ">
-						<button className=" w-48 rounded-sm
+					<div className="w-full flex items-center p-4 font-mono gap-4 
+									md:flex-row flex-col">
+						<button className=" w-full md:w-48 rounded-sm
 											flex items-center justify-center gap-2
 											border-2 border-amber-500 bg-amber-500 p-4 text-amber-50
-											">
+											"
+								onClick={() => {navigate("/welcome")}}>
 							<p>Join the suns</p>
 							<FiArrowRight/>
 						</button>
-						<button className=" w-48 rounded-sm
+						<button className=" w-full md:w-48 rounded-sm
 											flex items-center justify-center gap-2
 											border-2 border-amber-500 p-4 text-amber-500
-											">
+											"
+								onClick={() => {navigate("/auth")}}>
 							<p>Log in</p>
 							<FiArrowRight/>
 						</button>
